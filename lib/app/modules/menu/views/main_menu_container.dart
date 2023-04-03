d827a4aecb728cc9f7c63e903c08ab9fb66ede8c
Sean Tsang <9394wing@gmail.com>
2023-04-03 21:27:05 +0800

    add firebase hosting

--- lib/app/modules/menu/views/main_menu_container.dart
+++ lib/app/modules/menu/views/main_menu_container.dart
@@ -17,13 +17,12 @@ class MainMenuContainer extends StatelessWidget {
     BreathController breathController = Get.find<BreathController>();
     return Column(children: [
       Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           MenuItemView(
-            label:'Stregth',
-            icon: 'assets/images/strength.png',
-            action: () => router.changePage(Routes.STRENGTH_LIST)
-          ),
+              label: 'Stregth',
+              icon: 'assets/images/strength.png',
+              action: () => router.changePage(Routes.STRENGTH_LIST)),
         ],
       ),
       const SizedBox(
@@ -31,35 +30,32 @@ class MainMenuContainer extends StatelessWidget {
       ),
       Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         MenuItemView(
-          label:LocaleKeys.menu_Wheel.tr,
-          icon: 'assets/images/wheel.png',
-          action: () => router.changePage(Routes.WHEEL_OF_LIFE)
-        ),
+            label: LocaleKeys.menu_Wheel.tr,
+            icon: 'assets/images/wheel.png',
+            action: () => router.changePage(Routes.WHEEL_OF_LIFE)),
+        // MenuItemView(
+        //   label:LocaleKeys.menu_Smile.tr,
+        //   icon: 'assets/images/dream.png',
+        //   action : () => router.changePage(Routes.FACE_DETECTION)
+        // ),
         MenuItemView(
-          label:LocaleKeys.menu_Smile.tr,
-          icon: 'assets/images/dream.png',
-          action : () => router.changePage(Routes.FACE_DETECTION)
-        ),
-        MenuItemView(
-          label:LocaleKeys.menu_Dream.tr,
-          icon: 'assets/images/dream.png',
-          action : () => router.changePage(Routes.DREAM_LIST)
-        ),
-        // 
+            label: LocaleKeys.menu_Dream.tr,
+            icon: 'assets/images/dream.png',
+            action: () => router.changePage(Routes.DREAM_LIST)),
+        //
       ]),
       const SizedBox(
         height: 24.0,
       ),
       Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        // MenuItemView(
+        //     label: LocaleKeys.menu_breath.tr,
+        //     icon: 'assets/images/breath.png',
+        //     action: () => breathController.startBreath()),
         MenuItemView(
-            label: LocaleKeys.menu_breath.tr,
-            icon: 'assets/images/breath.png',
-            action: () => breathController.startBreath()),
-        MenuItemView(
-          label: LocaleKeys.menu_Emotion.tr,
-          icon: 'assets/images/emotion.png',
-          action: () => router.changePage(Routes.CONFRIM_EMOTION)
-        ),
+            label: LocaleKeys.menu_Emotion.tr,
+            icon: 'assets/images/emotion.png',
+            action: () => router.changePage(Routes.CONFRIM_EMOTION)),
         MenuItemView(
             label: LocaleKeys.menu_achievement.tr,
             icon: 'assets/images/achievement.png',
